9c907060e8bd3c986bdf22035ed5e7d1f9699a51
Thomas Kim <Thomasjk0831@gmail.com>
2020-09-11 11:29:57 -0400

    set up APP. need to set up action and reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import reducer from './reducer/index'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+    </Provider>, document.getElementById("root"));
---
+++ smurfs/src/reducer/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+    smurfs : []
+}
+
+export default (state=initialState, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
